b664c23b85eb66479db23978b278732bdb2ab0b8
Asmaathabet <asmathabet2009@hotmail.com>
2020-02-05 11:33:19 +0200

    create connection between server and client

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var socket = require('socket.io');
 
+// App setup
 var app = express();
-
 var server = app.listen(5000, ()=>{
     console.log('Server Listening on port 5000')
 })
 
-// static file 
+// static files
 app.use(express.static('public'));
+
+// socket setup
+var io = socket(server);
+
+io.on('connection', (socket) =>{
+    console.log('made socket connection', socket.id);
+})
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+// make connection with client side 
+var socket = io.connect('http://localhost:5000');
