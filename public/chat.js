e1b7687aa0f1fc566551913916b02c2a4291d237
Asmaathabet <asmathabet2009@hotmail.com>
2020-02-08 23:40:54 +0200

    emit message using dom elements in client side

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,8 @@
 // make connection with client side 
 var socket = io.connect('http://localhost:5000');
+
+// Dom
+var message = document.getElementById('message'),
+    sender = document.getElementById('sender'),
+    send = document.getElementById('send'),
+    output = document.getElementById('output');
