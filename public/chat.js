c36e17b343f270e5fb5b563c7985842e0809694e
Asmaathabet <asmathabet2009@hotmail.com>
2020-02-09 23:31:47 +0200

    clear feedback after pressing on send btn

--- public/chat.js
+++ public/chat.js
@@ -23,9 +23,10 @@ message.addEventListener('keypress', ()=>{
 
 // Listen for events
 socket.on('chat', data =>{
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>'+ data.sender+ ':</strong>'+ data.message + '</p>';
 })
 
 socket.on('typing', data =>{
-    feedback.innerHTML += '<p><em>'+ data+ 'is typing a message ... </em></p>';
+    feedback.innerHTML += '<p><em>'+ data+ ' is typing a message ... </em></p>';
 })
