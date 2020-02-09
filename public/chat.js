c82b96d503dcf6e1982a4488af4791ce626069c2
Asmaathabet <asmathabet2009@hotmail.com>
2020-02-09 23:24:05 +0200

    Broadcasting for messages

--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,8 @@ var socket = io.connect('http://localhost:5000');
 var message = document.getElementById('message'),
     sender = document.getElementById('sender'),
     send = document.getElementById('send'),
-    output = document.getElementById('output');
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
 // emit events 
 
@@ -14,9 +15,17 @@ send.addEventListener('click', ()=>{
       message : message.value ,
       sender: sender.value
     })
+}); 
+
+message.addEventListener('keypress', ()=>{
+    socket.emit('typing', sender.value);
 })
 
 // Listen for events
 socket.on('chat', data =>{
     output.innerHTML += '<p><strong>'+ data.sender+ ':</strong>'+ data.message + '</p>';
+})
+
+socket.on('typing', data =>{
+    feedback.innerHTML += '<p><em>'+ data+ 'is typing a message ... </em></p>';
 })
